7a15a6d7e5ce5ee81f59175e05417e9798a8b3d7
Yasin Karademir <yasin.krdemir@gmail.com>
2023-05-25 14:31:43 +0300

    End commit

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import tkinter as tk
 from tkinter import filedialog
-import networkx as nx
-from bokeh.palettes import Spectral4
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
-import matplotlib.pyplot as plt
 import networkx as nx
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
@@ -12,17 +8,14 @@ import mplcursors
 
 import re
 
-import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize, sent_tokenize
 from collections import defaultdict, Counter
 import string
 import ssl
-from gensim.models import Word2Vec
 from sklearn.metrics.pairwise import cosine_similarity
 from transformers import BertModel, BertTokenizer
 import nltk
-import torch
 import numpy as np
 import gensim.downloader as api
 
@@ -34,14 +27,11 @@ except AttributeError:
 else:
     ssl._create_default_https_context = _create_unverified_https_context
 
+from rouge import Rouge
+
 nltk.download("stopwords")
 nltk.download("punkt")
 nltk.download('averaged_perceptron_tagger')
-
-import warnings
-
-# warnings.filterwarnings("ignore")
-
 
 # Word2Vec modelini yükle
 word2vec_model = api.load("word2vec-google-news-300")
@@ -73,29 +63,8 @@ def sentence_similarity(sentence1, sentence2, model_word2vec, model_bert, tokeni
     sim_w2v = cosine_similarity([vector1_w2v], [vector2_w2v])[0][0]
     sim_bert = cosine_similarity(vector1_bert, vector2_bert)[0][0]
 
-    return (sim_w2v + sim_bert) / 2
-
-
-'''
-def sentence_similarity(sentence1, sentence2, model_bert, tokenizer_bert):
-    vector1_bert = bert_sentence_vector(sentence1, model_bert, tokenizer_bert)
-    vector2_bert = bert_sentence_vector(sentence2, model_bert, tokenizer_bert)
-
-    sim_bert = cosine_similarity([vector1_bert], [vector2_bert])[0][0]
+    # return (sim_w2v + sim_bert) / 2
     return sim_bert
-'''
-
-
-def sentence_similarity_dict(sentences_dict, model_word2vec, model_bert, tokenizer_bert):
-    similarities = {}
-    for i in sentences_dict:
-        similarities[i] = {}
-        for j in sentences_dict:
-            if i != j:
-                sim = sentence_similarity(sentences_dict[i]["sentence"], sentences_dict[j]["sentence"], model_word2vec,
-                                          model_bert, tokenizer_bert)
-                similarities[i][j] = sim
-    return similarities
 
 
 def create_graph_from_sentences(sentences):
@@ -198,7 +167,7 @@ def visualize_graph(G, parent_widget, sentences):
 
     pos = nx.spring_layout(G, k=0.3)
 
-    node_labels = {k: f'{k}: {v["score"]}' for k, v in sentences.items()}
+    node_labels = {k: f'{k}: {v["score"]:.4f}' for k, v in sentences.items()}
     nx.draw(G, pos, node_size=1000, ax=ax, labels=node_labels, font_size=10, font_weight='bold')
 
     canvas = FigureCanvasTkAgg(fig, master=parent_widget)
@@ -210,7 +179,8 @@ def visualize_graph(G, parent_widget, sentences):
         index = sel.index  # Düğümün indeksini elde edin (Selection.index kullanılarak)
         sentence = sentences[int(index)]["sentence"]  # İndeksi kullanarak cümleyi elde edin
         score = sentences[int(index)]["score"]  # İndeksi kullanarak puanı elde edin
-        sel.annotation.set_text(f"{sentence}\nPuan: {score}")  # Düğümde cümleyi ve puanı göster
+        # sel.annotation.set_text(f"{sentence}\nPuan: {score}")  # Düğümde cümleyi ve puanı göster
+        sel.annotation.set_text(f"{sentence}\nPuan: {score:.4f}")
 
     mplcursors.cursor(ax, hover=False).connect("add", show_sentence)
 
@@ -234,37 +204,19 @@ def select_file(root):
         sentences_dict = {}
         sentences_dict2 = {}
 
-        for i in sentences_input:
-            print(on_isleme(i))
-
         # başlık sentences_input[0]
         for idx, sentence in enumerate(sentences_input[1:]):
-            if (sentence.strip() != ""):
+            if sentence.strip() != "":
                 # ID, cümle ve cümle puanını (varsayılan 5) sentences_dict2'ye ekle
                 sentences_dict2[idx] = {"sentence": sentence.strip() + ".",
                                         "score": cumle_puani_hesapla(sentence.strip(), sentences_input[0],
                                                                      tema_kelimeler(expected_input))}
 
         for idx, sentence in enumerate(sentences_output):
-            if (sentence.strip() != ""):
+            if sentence.strip() != "":
                 sentences_dict[idx] = sentence.strip()
-    # -------------------------------------*deneme*-------------------------------------
-    '''
-    similarity_dict = {}
 
-    for i in range(len(sentences_dict2)):
-        for j in range(i + 1, len(sentences_dict2)):
-            similarity = sentence_similarity(sentences_dict2[i]['sentence'], sentences_dict2[j]['sentence'],
-                                             word2vec_model, bert_model, bert_tokenizer)
-            similarity_dict[(i, j)] = similarity
-
-    for pair, similarity in similarity_dict.items():
-        print(f"Cümle {pair[0]} ve Cümle {pair[1]} arasındaki benzerlik: {similarity}")
-    print(similarity_dict)
-    '''
-
-    # -------------------------------------*deneme*-------------------------------------
-
+    print(sentences_dict2)
     # benzerlik sözlüğünü oluştur
     similarity_dict = {}
     for i in range(len(sentences_dict2)):
@@ -275,20 +227,130 @@ def select_file(root):
             similarity_dict[(i, j)] = similarity
 
     # belirli bir eşik üzerindeki benzerliklere sahip cümleleri içeren bir graf oluştur
-    similarity_threshold = 0.6
+    similarity_threshold = float(similarity_entry.get())
     print(similarity_dict)
+
     G = create_similarity_graph(similarity_dict, similarity_threshold)
+
+    ozet = summarize(sentences_dict2, G, int(number_summary_sentences_entry.get()))
 
     # grafiği görselleştir
     visualize_similarity_graph(G)
 
     # -------------------------------------*deneme*-------------------------------------
 
-
-
-  # sentences_dict2'yi kullanarak grafiği oluştur
+    # sentences_dict2'yi kullanarak grafiği oluştur
     G = create_graph_from_sentences(sentences_dict2)
     visualize_graph(G, root, sentences_dict2)
+
+    display_summary(ozet, expected_output)
+
+
+def calculate_rouge_score(summary, reference):
+    rouge = Rouge()
+    scores = rouge.get_scores(summary, reference)
+    return scores
+
+
+def display_summary(summary, expected_output):
+    # Cümleleri birleştir
+    text = " ".join(summary)
+    print(text)
+
+    # Rouge skorunu hesaplama
+    rouge_score = calculate_rouge_score(text, expected_output)
+    print(rouge_score)
+
+    # Tkinter penceresi oluşturma
+    window = tk.Tk()
+    window.title("Özet")
+
+    # Metni bir etikette gösterme
+    summary_label = tk.Label(window, text=text, wraplength=600)
+    summary_label.pack()
+
+    # ROUGE skorunu bir etikette gösterme
+    rouge_label = tk.Label(window, text=f"ROUGE Score: {rouge_score}", wraplength=600)
+    rouge_label.pack()
+
+    # Pencereyi ekranda gösterme
+    window.mainloop()
+
+
+'''
+def display_summary(summary, expected_output):
+    # Cümleleri birleştir
+    text = " ".join(summary)
+    print(text)
+
+    # Tkinter penceresi oluşturma
+    window = tk.Tk()
+    window.title("Özet")
+
+    # Metni bir etikette gösterme
+    label = tk.Label(window, text=text, wraplength=600)
+    label.pack()
+    puan = calculate_rouge_score(text, expected_output)
+    print(puan)
+
+    # Pencereyi ekranda gösterme
+    window.mainloop()
+'''
+
+
+def summarize(sentences, G, top_n=5):
+    # Cümlelerin derecelerini hesapla
+    degrees = dict(G.degree(weight='weight'))
+
+    # Cümlelerin skorlarına göre dereceleri ayarla
+    adjusted_degrees = {index: degree * sentences[index]["score"] for index, degree in degrees.items()}
+
+    # Dereceleri sırala
+    ranked_sentences = sorted(((degree, index) for index, degree in adjusted_degrees.items()), reverse=True)
+
+    # En yüksek dereceli cümleleri özete ekle
+    summary = [sentences[index]["sentence"] for _, index in ranked_sentences[:top_n]]
+
+    return summary
+
+
+'''
+def summarize2(sentences, similarity_dict, top_n=5):
+    # Cümle skorlarını tutan bir sözlük oluştur
+    sentence_scores = {}
+
+    # Her cümle için, diğer tüm cümlelerle olan benzerliklerin toplamını hesapla
+    for pair, similarity in similarity_dict.items():
+        if pair[0] not in sentence_scores:
+            sentence_scores[pair[0]] = 0
+        if pair[1] not in sentence_scores:
+            sentence_scores[pair[1]] = 0
+        sentence_scores[pair[0]] += similarity
+        sentence_scores[pair[1]] += similarity
+
+    # Cümleleri skorlarına göre sırala
+    ranked_sentences = sorted(((score, index) for index, score in sentence_scores.items()), reverse=True)
+
+    # En yüksek skorlu cümleleri özete ekle
+    summary = [sentences[index]["sentence"] for _, index in ranked_sentences[:top_n]]
+
+    return summary
+'''
+
+'''
+def summarize1(sentences, G, top_n=5):
+    # PageRank algoritmasını uygula
+    pagerank_scores = nx.pagerank(G)
+
+    # Cümleleri PageRank skorlarına göre sırala
+    ranked_sentences = sorted(((score, index) for index, score in pagerank_scores.items()), reverse=True)
+
+    # En yüksek skorlu cümleleri özete ekle
+    summary = [sentences[index]["sentence"] for _, index in ranked_sentences[:top_n]]
+
+    return summary
+    
+'''
 
 
 def create_similarity_graph(similarity_dict, threshold):
@@ -309,6 +371,38 @@ def visualize_similarity_graph(G):
     new_window.iconbitmap("icon.ico")
     new_window.wm_attributes("-alpha", 0.9)
 
+    fig = Figure(figsize=(100, 100))
+
+    ax = fig.add_subplot(111)
+
+    pos = nx.spring_layout(G)
+
+    # Düğüm etiketlerini oluştur (düğüm ismi ve derece)
+    node_labels = {node: f"{node} ({G.degree(node)})" for node in G.nodes()}
+
+    nx.draw(G, pos, labels=node_labels, ax=ax)
+
+    # kenar etiketlerini ekle
+    edge_labels = nx.get_edge_attributes(G, 'weight')
+    # Etiketleri uygun biçime getir
+    for key in edge_labels:
+        edge_labels[key] = f"{edge_labels[key]:.3f}"  # 3 basamak için yuvarlama
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, ax=ax)
+
+    canvas = FigureCanvasTkAgg(fig, master=new_window)
+    canvas.draw()
+    canvas.get_tk_widget().pack()
+
+
+'''
+def visualize_similarity_graph(G):
+    new_window = tk.Toplevel(root)
+    new_window.geometry("500x500")
+    new_window.title("Benzerlik Grafiği")
+    new_window.config(background="#000000")
+    new_window.iconbitmap("icon.ico")
+    new_window.wm_attributes("-alpha", 0.9)
+
     fig = Figure(figsize=(30, 30))
 
     ax = fig.add_subplot(111)
@@ -319,7 +413,7 @@ def visualize_similarity_graph(G):
     canvas = FigureCanvasTkAgg(fig, master=new_window)
     canvas.draw()
     canvas.get_tk_widget().pack()
-
+'''
 
 root = tk.Tk()
 
@@ -330,6 +424,16 @@ root.title("Dosya Yükleme Uygulaması")
 root.config(background="#000000")
 root.iconbitmap("icon.ico")
 root.wm_attributes("-alpha", 0.9)
+
+similarity_label = tk.Label(root, text="Similarity Threshold:")
+similarity_label.pack()
+similarity_entry = tk.Entry(root)
+similarity_entry.pack()
+
+number_summary_sentences = tk.Label(root, text="Number of Summary Sentences:")
+number_summary_sentences.pack()
+number_summary_sentences_entry = tk.Entry(root)
+number_summary_sentences_entry.pack()
 
 button = tk.Button(root, text="Dosya Yükle", command=lambda: select_file(root))
 
